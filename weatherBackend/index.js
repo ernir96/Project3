c6243a22f9fb0a1ae77c7b91e133b354db4400f7
ernir96 <ernir17@ru.is>
2019-03-11 19:52:14 +0000

    added files provided by teacher

---
+++ weatherBackend/index.js
@@ -0,0 +1,15 @@
+//Sample data for Project 3
+
+//The following is an example of an array of two stations. 
+//The observation array includes the ids of the observations belonging to the specified station
+var stations = [
+    {id: 1, description: "Reykjavik", lat: 64.1275, lon: 21.9028, observations: [2]},
+    {id: 422, description: "Akureyri", lat: 65.6856, lon: 18.1002, observations: [1]}
+];
+
+//The following is an example of an array of two observations.
+//Note that an observation does not know which station it belongs to!
+var observations = [
+    {id: 1, date: 1551885104266, temp: -2.7, windSpeed: 2.0, windDir: "ese", prec: 0.0, hum: 82.0},
+    {id: 2, date: 1551885137409, temp: 0.6, windSpeed: 5.0, windDir: "n", prec: 0.0, hum: 50.0},
+];
