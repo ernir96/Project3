a7a1a11c40c8d903aa303d32fea1211f54c43c25
Bjarturl <bjartur17@ru.is>
2019-03-12 11:13:36 +0000

    post ::)

--- weatherBackend/index.js
+++ weatherBackend/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
 //The following is an example of an array of two stations. 
 //The observation array includes the ids of the observations belonging to the specified station
 var stations = [
     {id: 1, description: "Reykjavik", lat: 64.1275, lon: 21.9028, observations: [2]},
-    {id: 422, description: "Akureyri", lat: 65.6856, lon: 18.1002, observations: [1]}
+    {id: 2, description: "Akureyri", lat: 65.6856, lon: 18.1002, observations: [1]}
 ];
+var stationId = stations.length;
 
 //The following is an example of an array of two observations.
 //Note that an observation does not know which station it belongs to!
@@ -24,7 +27,7 @@ app.get('/stations', (req, res) => {
 
 app.get('/stations/:id', (req, res) => {
     for(let i = 0; i < stations.length; i++){
-        if(stations[i].id === req.params.id) {
+        if(stations[i].id == req.params.id) {
             res.status(200).json(stations[i]);
             return;
         }
@@ -35,13 +38,21 @@ app.get('/stations/:id', (req, res) => {
 });
 
 app.post('/stations', (req, res) => {
-    if(req.body === NULL || req.body.description === undefined || req.body.lat === undefined || req.body.lon === undefined){
+    if(req.body === null || req.body.description === undefined || req.body.lat === undefined || req.body.lon === undefined || req.body.observations === undefined){
         res.status(404).json({
             'message': "Missing station information!"
         }); 
+    } else {
+        let newStation = {
+            id: ++stationId,
+            description: req.body.description,
+            lat: req.body.lat,
+            lon: req.body.lon,
+            observations: req.body.observations
+            }
+        stations.push(newStation);
+        res.status(201).json(newStation);
     }
-    
-    res.status(200).json({'message': "Create a new station"}); 
 });
 
 app.delete('/stations/:id', (req, res) => {
