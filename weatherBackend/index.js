6a569ee41ffb0590cdf9c1e827ee5d8ed9d4fba5
illugis <illugi17@ru.is>
2019-03-12 15:35:09 +0000

    fullt af breytingum í observations skeleton

--- weatherBackend/index.js
+++ weatherBackend/index.js
@@ -11,19 +11,21 @@ var stations = [
     {id: 1, description: "Reykjavik", lat: 64.1275, lon: 21.9028, observations: [2]},
     {id: 2, description: "Akureyri", lat: 65.6856, lon: 18.1002, observations: [1]}
 ];
-var stationId = stations.length;
-var observationId = observations.length;
-
-//The following is an example of an array of two observations.
-//Note that an observation does not know which station it belongs to!
 var observations = [
     {id: 1, date: 1551885104266, temp: -2.7, windSpeed: 2.0, windDir: "ese", prec: 0.0, hum: 82.0},
     {id: 2, date: 1551885137409, temp: 0.6, windSpeed: 5.0, windDir: "n", prec: 0.0, hum: 50.0},
 ];
+var stationId = stations.length;
+var observationId = observations.length;
+
 
 //Stations
 app.get('/stations', (req, res) => {
-    res.status(200).json(stations); 
+    var temp = [];
+    for(var i = 0; i < stations.length; i++){
+        temp.push({id: stations[i].id, description: stations[i].description});
+    }
+    res.status(200).json(temp); 
 });
 
 app.get('/stations/:id', (req, res) => {
@@ -96,6 +98,131 @@ app.delete('/stations', (req, res) => {
     var returnArr = stations.slice();
     stations = [];
     res.status(200).json(returnArr);
+});
+
+/*app.listen(port, hostname, () => {
+    console.log('Express app listening on port ' + port);
+});*/
+
+// Observations
+app.get('/observations/:station', (req, res) => {
+    temp = [];
+    var station = Number(req.params.station);
+    console.log(station);
+    for(var i = 0; i < stations.length; i++){
+        if(station === stations[i].id){
+            for(var j = 0; j < stations[i].observations.length; j++){
+                console.log("length: " + stations[i].observations.length);
+                for(var k = 0; k < observations.length; k++){
+                    console.log(observations[k]);
+                    console.log(stations[i].observations[j]);
+                    if(observations[k].id == stations[i].observations[j]){
+                        //console.log(stations[i].observations[j]);
+                        temp.push(observations[k]);
+                        console.log(observations[k]);
+                    }
+                }
+                //temp.push(observations[stations[i].observations[j]]);
+            }
+            res.status(200).json(temp);
+        }
+    }
+    res.status(404).json({message: "No station with id: " + req.params.station});
+});
+
+app.get('/observations/:station/:id', (req, res) => {
+    var id = Number(req.params.id);
+    var station = Number(req.params.station);
+    var observationFound = null;
+    console.log(id);
+    console.log(observations.length);
+    for(var i = 0; i < stations.length; i++){
+        for(var j = 0; j < stations[i].observations.length; j++){
+            if(station === stations[i].id && id == stations[i].observations[j]){
+                for(var k = 0; k < observations.length; k++){
+                    if(observations[k].id === id){
+                        res.status(200).json(observations[k]);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    res.status(404).json({message: "No obeservation with id: " + id});
+});
+
+app.post('/observations/:station', (req, res) => {
+    if(req.body === null || req.body.temp === undefined || req.body.windSpeed === undefined || 
+        req.body.windDir === undefined || req.body.prec === undefined || req.body.hum === undefined){
+        res.status(404).json({'message': "Missing station information!"});
+    }
+    else{
+        var newObservation = {
+            id: ++observationId,
+            date: Date.now(),
+            temp: req.body.temp,
+            windSpeed: req.body.windSpeed,
+            windDir: req.body.windDir,
+            prec: req.body.prec,
+            hum: req.body.hum
+        }
+        console.log(newObservation.id);
+        for(var i = 0; i < stations.length; i++){
+            if(req.params.station == stations[i].id){
+                stations[i].observations.push(newObservation.id);
+            }
+        }
+        observations.push(newObservation);
+        res.status(201).json(newObservation);
+    }
+});
+
+app.delete('/observations/:station/:id', (req, res) => {
+    var id = Number(req.params.id);
+    var station = Number(req.params.station);
+    for(var i = 0; i < stations.length; i++){
+        for(var j = 0; j < stations[i].observations.length; j++){
+            if(station === stations[i].id && id == stations[i].observations[j]){
+                stations[i].observations.splice(j, 1);
+            }
+        }
+    }
+    for(var k = 0; k < observations.length; k++){
+        if(observations[k].id == id){
+            var delOb = observations.splice(k, 1);
+            res.status(200).json(delOb);
+            return;
+        }
+    }
+    res.status(404).json({message: "No obeservation with id: " + id + " in station with id: " + station});
+});
+
+app.delete('/observations/:station', (req, res) => {
+    var station = req.params.station;
+    var temp = [];
+    for(var i = 0; i < stations.length; i++){
+        if(station == stations[i].id){
+            //console.log(stations[i].id);
+            for(var j = 0; j < stations[i].observations.length; j++){
+                for(var k = 0; k < observations.length; k++){
+                    //console.log(stations[i].observations);
+                    //console.log(stations[i].observations[j]);
+                    //console.log(observations[k]);
+                    if(stations[i].observations[j] == observations[k].id){
+                        //console.log(observations[k].);
+                        var deletedOb = observations.splice(k, 1);
+                        temp.push(deletedOb);
+                    }
+                }
+            }
+        }
+    }
+    for(var i = 0; i < stations.length; i++){
+        if(station == stations[i].id){
+            stations[i].observations = [];
+        }
+    }
+    res.status(200).json(temp);
 });
 
 app.listen(port, hostname, () => {
